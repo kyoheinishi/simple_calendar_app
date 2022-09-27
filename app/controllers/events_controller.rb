98e26d8ac25408407ddac72a3d220ee5dd8739e0
kyoheinishi <kyohei.nishi@gmail.com>
2022-09-27 15:19:01 +0900

    createアクション

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,15 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    Event.create(event_parameter)
+    redirect_to root_path
+  end
+
+  private
+
+  def event_parameter
+    params.require(:event).permit(:title, :content, :start_time)
+  end
+
 end
